c143df334d3967f79b9e6e40bc96a9cd42d478f6
Matt Ferris <matt@bueller.ca>
2015-06-03 17:54:50 -0700

    Added boilerplate to BundleInterface.php

--- src/BundleInterface.php
+++ src/BundleInterface.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * DI - A dependency injection library for PHP
+ * www.bueller.ca/di
+ *
+ * BundleInterface.php
+ * @copyright Copyright (c) 2015 Matt Ferris
+ * @author Matt Ferris <matt@bueller.ca>
+ *
+ * Licensed under BSD 2-clause license
+ * www.bueller.ca/di/license
+ */
+
 namespace MattFerris\Di;
 
 interface BundleInterface
